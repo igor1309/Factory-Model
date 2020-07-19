0437f167ff2b25abc47647059df47aafad6c0be9
Igor Malyarov <igor1309@gmail.com>
2020-07-20 00:37:46 +0300

    Initial Commit

---
+++ Factory Model/Factory_ModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Factory_ModelApp.swift
+//  Factory Model
+//
+//  Created by Igor Malyarov on 20.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct Factory_ModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
