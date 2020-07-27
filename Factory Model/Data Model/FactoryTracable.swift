28d4899b5736e2b3f31090e67b9e6bc1f0c8d894
Igor Malyarov <igor1309@gmail.com>
2020-07-27 19:07:00 +0300

    FactoryTracable

--- Factory Model/Data Model/FactoryTracable.swift
+++ Factory Model/Data Model/FactoryTracable.swift
@@ -16,10 +16,6 @@ protocol FactoryTracable {
 }
 extension FactoryTracable where Self: NSManagedObject {
     static func factoryPredicate(for factory: Factory) -> NSPredicate {
-        //  MARK: - not type-safe
-        /// see Building compile-time type-checked NSPredicate using KeyPath and operators in Swift | by Lazar Otasevic | Medium
-        /// https://medium.com/@redhotbits/building-nspredicate-with-swift-keypaths-and-operators-5ff569db304f
-        
         NSPredicate(
             format: format, (factoryPath), factory
         )
@@ -27,10 +23,20 @@ extension FactoryTracable where Self: NSManagedObject {
 }
 
 extension Feedstock: FactoryTracable {
-    @objc static var factoryPath: String {
-        "\(#keyPath(Feedstock.ingredients_))"
-    }
     static var format: String {
         "ANY %K.base.factory == %@"
+    }
+    static var factoryPath: String {
+            "\(#keyPath(Feedstock.ingredients_))"
+    }
+}
+
+extension Sales: FactoryTracable {
+    static var format: String {
+        "%K == %@"
+    }
+
+    static var factoryPath: String {
+        "\(#keyPath(Sales.product.base.factory))"
     }
 }
