4526ba4a48bf0c02a7eb7a36b3bd15a8125897f4
Igor <igor1309@gmail.com>
2020-08-16 14:16:37 +0300

    idea

--- Factory Model/Editor Core/NameEditor.swift
+++ Factory Model/Editor Core/NameEditor.swift
@@ -1,6 +1,17 @@
 import SwiftUI
 
-call: NameEditor(entity: T(context: context))
+call: 
+
+NavigationLink(
+    destination: NameEditor(entity: T(context: context)),
+    isPresented: isPresented) {
+        EmptyView()
+        или
+        Button("New") {
+            isPresented = true
+        }
+    }
+
 
 struct NameEditor<T: NSManagedObject & Namable>: View {
     @ObservedObject var entity: T
