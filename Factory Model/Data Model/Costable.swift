c7ecb48a8598bc239b4863cdcf519d45b82ff913
Igor Malyarov <igor1309@gmail.com>
2020-09-16 11:33:33 +0300

    protocol Costable tweaks

--- Factory Model/Data Model/Costable.swift
+++ Factory Model/Data Model/Costable.swift
@@ -13,6 +13,52 @@ protocol Costable {
     func productionCost(in period: Period) -> Cost
 }
 
+extension Costable where Self: CustomUnitable & Productable & Ingredientable & Salarable & Depreciable & Utilizable {
+    func unitCost(in period: Period) -> Cost {
+        Cost(
+            title: "Unit Cost per \(customUnitString)",
+            header: "Unit Cost",
+            hasDecimal: true,
+            ingredientCostExVAT: ingredientsExVAT(in: period),
+            salaryWithTax: salaryWithTax(in: period),
+            depreciation: depreciationWithTax(in: period),
+            utilityCostExVAT: utilitiesExVAT(in: period)
+        )
+    }
+
+    func salesCost(in period: Period) -> Cost {
+        let ingredientCostExVAT = salesQty(in: period) * ingredientsExVAT(in: period)
+        let salary =              salesQty(in: period) * salaryWithTax(in: period)
+        let depreciation =        salesDepreciationWithTax(in: period)
+        let utilityCostExVAT =    utilitiesExVAT(in: period)
+        
+        return Cost(
+            title: "COGS",
+            header: "Sales Cost Structure",
+            ingredientCostExVAT: ingredientCostExVAT,
+            salaryWithTax: salary,
+            depreciation: depreciation,
+            utilityCostExVAT: utilityCostExVAT
+        )
+    }
+    
+    func productionCost(in period: Period) -> Cost {
+        let ingredientCostExVAT = productionQty(in: period) * ingredientsExVAT(in: period)
+        let salary =              productionQty(in: period) * salaryWithTax(in: period)
+        let depreciation =        productionDepreciationWithTax(in: period)
+        let utilityCostExVAT =    productionUtilitiesExVAT(in: period)
+        
+        return Cost(
+            title: "Production Cost",
+            header: "Production Cost Structure",
+            ingredientCostExVAT: ingredientCostExVAT,
+            salaryWithTax: salary,
+            depreciation: depreciation,
+            utilityCostExVAT: utilityCostExVAT
+        )
+    }
+}
+
 extension Factory: Costable {
     //  MARK: - FINISH THIS
     func unitCost(in period: Period) -> Cost {
@@ -59,94 +105,5 @@ extension Factory: Costable {
     }
 }
 
-extension Base: Costable {
-    func unitCost(in period: Period) -> Cost {
-        Cost(
-            title: "Unit Cost per \(customUnitString)",
-            header: "Unit Cost",
-            hasDecimal: true,
-            ingredientCostExVAT: ingredientsExVAT(in: period),
-            salaryWithTax: salaryWithTax(in: period),
-            depreciation: depreciationWithTax(in: period),
-            utilityCostExVAT: utilitiesExVAT(in: period)
-        )
-    }
-    
-    func salesCost(in period: Period) -> Cost {
-        let ingredientCostExVAT = salesQty(in: period) * ingredientsExVAT(in: period)
-        let salary =              salesQty(in: period) * salaryWithTax(in: period)
-        let depreciation =        salesDepreciationWithTax(in: period)
-        let utilityCostExVAT =    utilitiesExVAT(in: period)
-        
-        return Cost(
-            title: "COGS",
-            header: "Sales Cost Structure",
-            ingredientCostExVAT: ingredientCostExVAT,
-            salaryWithTax: salary,
-            depreciation: depreciation,
-            utilityCostExVAT: utilityCostExVAT
-        )
-    }
-    
-    func productionCost(in period: Period) -> Cost {
-        let ingredientCostExVAT = productionQty(in: period) * ingredientsExVAT(in: period)
-        let salary =              productionQty(in: period) * salaryWithTax(in: period)
-        let depreciation =        productionDepreciationWithTax(in: period)
-        let utilityCostExVAT =    utilitiesExVAT(in: period)
-        
-        return Cost(
-            title: "Production Cost",
-            header: "Production Cost Structure",
-            ingredientCostExVAT: ingredientCostExVAT,
-            salaryWithTax: salary,
-            depreciation: depreciation,
-            utilityCostExVAT: utilityCostExVAT
-        )
-    }
-}
-
-extension Product: Costable {
-    func unitCost(in period: Period) -> Cost {
-        Cost(
-            title: "Unit Cost per \(customUnitString)",
-            header: "Unit Cost",
-            hasDecimal: true,
-            ingredientCostExVAT: ingredientsExVAT(in: period),
-            salaryWithTax: salaryWithTax(in: period),
-            depreciation: depreciationWithTax(in: period),
-            utilityCostExVAT: utilitiesExVAT(in: period)
-        )
-    }
-    
-    func salesCost(in period: Period) -> Cost {
-        let ingredientCostExVAT = salesQty(in: period) * ingredientsExVAT(in: period)
-        let salary =              salesQty(in: period) * salaryWithTax(in: period)
-        let depreciation =        salesDepreciationWithTax(in: period)
-        let utilityCostExVAT =    utilitiesExVAT(in: period)
-        
-        return Cost(
-            title: "COGS",
-            header: "Sales Cost Structure",
-            ingredientCostExVAT: ingredientCostExVAT,
-            salaryWithTax: salary,
-            depreciation: depreciation,
-            utilityCostExVAT: utilityCostExVAT
-        )
-    }
-    
-    func productionCost(in period: Period) -> Cost {
-        let ingredientCostExVAT = productionQty(in: period) * ingredientsExVAT(in: period)
-        let salary =              productionQty(in: period) * salaryWithTax(in: period)
-        let depreciation =        productionDepreciationWithTax(in: period)
-        let utilityCostExVAT =    productionUtilitiesExVAT(in: period)
-        
-        return Cost(
-            title: "Production Cost",
-            header: "Production Cost Structure",
-            ingredientCostExVAT: ingredientCostExVAT,
-            salaryWithTax: salary,
-            depreciation: depreciation,
-            utilityCostExVAT: utilityCostExVAT
-        )
-    }
-}
+extension Base: Costable {}
+extension Product: Costable {}
