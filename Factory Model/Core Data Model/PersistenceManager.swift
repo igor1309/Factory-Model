614f9cef6105fe2f188cc16e2cc5878bef6435e4
Igor Malyarov <igor1309@gmail.com>
2020-10-23 23:22:30 +0300

    PersistenceManager tweaks

--- Factory Model/Core Data Model/PersistenceManager.swift
+++ Factory Model/Core Data Model/PersistenceManager.swift
@@ -30,6 +30,7 @@ class PersistenceManager: ObservableObject {
         return container
     }()
     
+    
     init(containerName: String, inMemory: Bool = false) {
         self.containerName = containerName
         self.inMemory = inMemory
@@ -45,9 +46,74 @@ class PersistenceManager: ObservableObject {
             }
         }
     }
+}
+
+extension PersistenceManager {
     
+    func deleteAll() {
+        let fetchRequest1: NSFetchRequest<NSFetchRequestResult> = Base.fetchRequest()
+        let batchDeleteRequest1 = NSBatchDeleteRequest(fetchRequest: fetchRequest1)
+        _ = try? container.viewContext.execute(batchDeleteRequest1)
+        
+        let fetchRequest2: NSFetchRequest<NSFetchRequestResult> = Buyer.fetchRequest()
+        let batchDeleteRequest2 = NSBatchDeleteRequest(fetchRequest: fetchRequest2)
+        _ = try? container.viewContext.execute(batchDeleteRequest2)
+        
+        let fetchRequest3: NSFetchRequest<NSFetchRequestResult> = Department.fetchRequest()
+        let batchDeleteRequest3 = NSBatchDeleteRequest(fetchRequest: fetchRequest3)
+        _ = try? container.viewContext.execute(batchDeleteRequest3)
+        
+        let fetchRequest4: NSFetchRequest<NSFetchRequestResult> = Division.fetchRequest()
+        let batchDeleteRequest4 = NSBatchDeleteRequest(fetchRequest: fetchRequest4)
+        _ = try? container.viewContext.execute(batchDeleteRequest4)
+        
+        let fetchRequest5: NSFetchRequest<NSFetchRequestResult> = Employee.fetchRequest()
+        let batchDeleteRequest5 = NSBatchDeleteRequest(fetchRequest: fetchRequest5)
+        _ = try? container.viewContext.execute(batchDeleteRequest5)
+        
+        let fetchRequest6: NSFetchRequest<NSFetchRequestResult> = Equipment.fetchRequest()
+        let batchDeleteRequest6 = NSBatchDeleteRequest(fetchRequest: fetchRequest6)
+        _ = try? container.viewContext.execute(batchDeleteRequest6)
+        
+        let fetchRequest7: NSFetchRequest<NSFetchRequestResult> = Expenses.fetchRequest()
+        let batchDeleteRequest7 = NSBatchDeleteRequest(fetchRequest: fetchRequest7)
+        _ = try? container.viewContext.execute(batchDeleteRequest7)
+        
+        let fetchRequest8: NSFetchRequest<NSFetchRequestResult> = Factory.fetchRequest()
+        let batchDeleteRequest8 = NSBatchDeleteRequest(fetchRequest: fetchRequest8)
+        _ = try? container.viewContext.execute(batchDeleteRequest8)
+        
+        let fetchRequest9: NSFetchRequest<NSFetchRequestResult> = Ingredient.fetchRequest()
+        let batchDeleteRequest9 = NSBatchDeleteRequest(fetchRequest: fetchRequest9)
+        _ = try? container.viewContext.execute(batchDeleteRequest9)
+        
+        let fetchRequest10: NSFetchRequest<NSFetchRequestResult> = Packaging.fetchRequest()
+        let batchDeleteRequest10 = NSBatchDeleteRequest(fetchRequest: fetchRequest10)
+        _ = try? container.viewContext.execute(batchDeleteRequest10)
+        
+        let fetchRequest11: NSFetchRequest<NSFetchRequestResult> = Product.fetchRequest()
+        let batchDeleteRequest11 = NSBatchDeleteRequest(fetchRequest: fetchRequest11)
+        _ = try? container.viewContext.execute(batchDeleteRequest11)
+        
+        let fetchRequest12: NSFetchRequest<NSFetchRequestResult> = Recipe.fetchRequest()
+        let batchDeleteRequest12 = NSBatchDeleteRequest(fetchRequest: fetchRequest12)
+        _ = try? container.viewContext.execute(batchDeleteRequest12)
+        
+        let fetchRequest13: NSFetchRequest<NSFetchRequestResult> = Sales.fetchRequest()
+        let batchDeleteRequest13 = NSBatchDeleteRequest(fetchRequest: fetchRequest13)
+        _ = try? container.viewContext.execute(batchDeleteRequest13)
+        
+        let fetchRequest14: NSFetchRequest<NSFetchRequestResult> = Utility.fetchRequest()
+        let batchDeleteRequest14 = NSBatchDeleteRequest(fetchRequest: fetchRequest14)
+        _ = try? container.viewContext.execute(batchDeleteRequest14)
+    }
+    
+    
+    //  MARK: - Sample Data
     
     func createSampleData() throws {
+        //deleteAll()
+
         let _ = Factory.createFactory1(in: context)
         let _ = Factory.createFactory2(in: context)
         
